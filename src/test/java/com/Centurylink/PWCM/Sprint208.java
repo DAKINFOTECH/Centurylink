69689f2035f8cd6cfb5a2d88d0a7d13f3578a363
arund87 <arund87@gmail.com>
2016-05-20 16:30:26 +0530

    Sprint108 Completed.

--- src/test/java/com/Centurylink/PWCM/Sprint208.java
+++ src/test/java/com/Centurylink/PWCM/Sprint208.java
@@ -3,6 +3,9 @@ package com.Centurylink.PWCM;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,6 +13,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
 import com.Centurylink.PWCM.pwcmlibrary;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -23,16 +27,19 @@ public class Sprint208 {
 	String reportpath=".\\Reporting\\Advreportpath.html";
 	String injectpath=".\\src\\main\\resources\\Injectfile.xls";
 	String Attachpath="C:\\Users\\348027\\git\\Centurylink";
+	String username="admin";
+	String password="admin";
 	
 	String Homeimg="//img[@src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTy6yBiky4iWbqq2qnPPYT7X8CQA_o_GGWbiZvtW5IapfIcbd_C']";
 	String resourceimg="//img[@src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSnXtWT2sNGRvc4T7I_SgH88-xewD8j0L9q6lrwUvMA2HSFJkAmLw']";
 	String productandsoluimg="//img[@src='http://www.localinternetservice.com/images/localinternet/centurylink-brand/devices.jpg']";
+	String copyright="    © 2016 CenturyLink. All Rights Reserved.";
 	
 	    @Test(priority=0)		
 	    public void UtilityLinks(){
 		extent=new ExtentReports(reportpath,true);
 		test=extent.startTest("Global Header-Utility Links");
-		test.assignCategory("Sprint108","Regression Test");
+		test.assignCategory("Sprint108");
 		driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -42,8 +49,8 @@ public class Sprint208 {
 		driver.findElement(By.id("submit-button")).isDisplayed();
 		
 		/*PWCM login*/
-		driver.findElement(By.id("username")).sendKeys("admin");
-		driver.findElement(By.id("password")).sendKeys("admin");
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
 		driver.findElement(By.id("submit-button")).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		test.log(LogStatus.PASS, "Login Success");
@@ -124,21 +131,18 @@ public class Sprint208 {
 		driver.findElement(By.linkText("SalesForce")).isDisplayed();
 		test.log(LogStatus.PASS, "Sales Force link is present");
 		
-		test.log(LogStatus.PASS, "UtilityLinks Test Case is Passed");
+		test.log(LogStatus.PASS, "UtilityLinks Test Passed");
 		extent.endTest(test);
 		extent.flush();
 	}
 	    
 	    @Test(priority=1)		
-	    public void Quicklinks(){
-	    System.out.println("log1");
+	    public void QuickLinks(){
 	    driver.get("http://172.26.130.130:4502/content/pwcm-first-page/home.html");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		test=extent.startTest("Global Footer-Quick Links");
-		test.assignCategory("Sprint108","Regression Test");
-		System.out.println("log2");
+		test.assignCategory("Sprint108");
 		test.log(LogStatus.PASS, "pwcmint page is ready");
-		System.out.println("log3");
 		/*User can see a section containing quick links to frequently used pages within Products and Resources sections */
 		driver.findElement(By.linkText("Home")).isDisplayed();
 		driver.findElement(By.linkText("Products and Solutions")).isDisplayed();
@@ -154,11 +158,91 @@ public class Sprint208 {
 		driver.findElement(By.xpath(Homeimg)).isDisplayed();
 		test.log(LogStatus.PASS, "Links are functional, navigation works properly");
 		
-		test.log(LogStatus.PASS, "QuickLinks Test Case is Passed");
+		/* Links TBD my content manager and editable by content manager*/
+		test.log(LogStatus.INFO, "Links TBD are editable by content manager");
+		
+		/* User can see a section containing copyright statement, use current partner site (© 2016, CenturyLink. All Rights Reserved)*/
+		driver.findElement(By.linkText("Products and Solutions")).click();
+		Assert.assertEquals(copyright, driver.findElement(By.xpath("//p[@class='small']")).getText());
+		driver.findElement(By.linkText("Resources")).click();
+		Assert.assertEquals(copyright, driver.findElement(By.xpath("//p[@class='small']")).getText());
+		driver.findElement(By.linkText("Home")).click();
+		Assert.assertEquals(copyright, driver.findElement(By.xpath("//p[@class='small']")).getText());
+		test.log(LogStatus.PASS, "Copyright statement is present on the page");
+		test.log(LogStatus.PASS, "Quiclinks Test Passed");
 		extent.endTest(test);
 		extent.flush();
 	    }
-	
+	    
+	    
+		@Test(priority=2)		
+		public void HomePage(){
+			driver.get("http://172.26.130.130:4502/content/pwcm-first-page/home.html");
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			test=extent.startTest("Global Header-Home Page");
+			test.assignCategory("Sprint108");
+			test.log(LogStatus.PASS, "pwcmint page is ready");
+			
+			/*User is able to land on a home page*/
+			driver.findElement(By.linkText("Home")).click();
+			driver.findElement(By.xpath(Homeimg)).isDisplayed();
+			test.log(LogStatus.PASS, "Home page is accessible by user");
+			
+			/*User is able to see the global header, footer, landing page body*/
+			driver.findElement(By.className("dropdown-toggle")).isDisplayed();
+			driver.findElement(By.linkText("SalesForce")).isDisplayed();
+			driver.findElement(By.linkText("Help")).isDisplayed();
+			driver.findElement(By.linkText("Feedback")).isDisplayed();
+			driver.findElement(By.linkText("Logout")).isDisplayed();
+			driver.findElement(By.xpath(Homeimg)).isDisplayed();
+			driver.findElement(By.className("ctlbiz-logo")).isDisplayed();
+			driver.findElement(By.className("search_textBox")).isDisplayed();
+			driver.findElement(By.xpath("//div[@class='dropdown-toggle active']")).isDisplayed();
+			driver.findElement(By.className("corp-logo")).isDisplayed();
+			driver.findElement(By.linkText("Home")).isDisplayed();
+			driver.findElement(By.linkText("Contact Us")).isDisplayed();
+			driver.findElement(By.linkText("Site Map")).isDisplayed();
+			driver.findElement(By.linkText("Policies")).isDisplayed();
+			test.log(LogStatus.PASS, "Global header/footer/landing page body are present");
+			
+			/*User see Home tab is bolded as active tab*/
+			driver.findElement(By.xpath("//span[@class='nav-home' and @style='font-weight: bolder;']")).isDisplayed();
+			test.log(LogStatus.PASS, "Home tab is bolded as active tab");
+					
+			test.log(LogStatus.PASS, "HomePage Test Passed");
+			extent.endTest(test);
+			extent.flush();
+		}
+		
+		@Test(priority=3)		
+		public void NavigationBar(){
+			driver.get("http://172.26.130.130:4502/content/pwcm-first-page/home.html");
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			test=extent.startTest("Global Header - Navigation Bar");
+			test.assignCategory("Sprint108");
+			test.log(LogStatus.PASS, "pwcmint page is ready");
+			
+			/* User can see Navigation tabs-Home, Products and Solutions, Resources*/
+			driver.findElement(By.linkText("Home")).isDisplayed();
+			driver.findElement(By.linkText("Products and Solutions")).isDisplayed();
+			driver.findElement(By.linkText("Resources")).isDisplayed();
+			test.log(LogStatus.PASS, "Navigation tabs (Home, Products and Solutions, Resources) are present");
+			
+			/*User sees Bolded tab for active tab*/
+			driver.findElement(By.linkText("Products and Solutions")).click();
+			driver.findElement(By.xpath("//span[@class='nav-products' and @style='font-weight: bold;']")).isDisplayed();
+			driver.findElement(By.linkText("Resources")).click();
+			driver.findElement(By.xpath("//span[@class='nav-resources' and @style='font-weight: bold;']")).isDisplayed();
+			driver.findElement(By.linkText("Home")).click();
+			driver.findElement(By.xpath("//span[@class='nav-home' and @style='font-weight: bolder;']")).isDisplayed();
+			test.log(LogStatus.PASS,"Bolded tab for active tab (clicked tab) is present");
+			test.log(LogStatus.PASS,"Home tab is clickable and returns the user to the Home page");
+			
+			test.log(LogStatus.PASS, "NavigationBar Test Passed");
+			extent.endTest(test);
+			extent.flush();
+		}
+	   
 	@AfterMethod
 	public void tearDown(ITestResult result) throws InterruptedException{
 	if(ITestResult.FAILURE==result.getStatus())
@@ -175,7 +259,7 @@ public class Sprint208 {
     @AfterClass
     public void closebrowser()
     {
-    	System.out.println("Testpassed");
+    	
      driver.get("C:\\Users\\348027\\git\\Centurylink\\Reporting\\Advreportpath.html");
     }
 
