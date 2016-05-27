00d34f15d53e1d960092a258f41ab079803e2dc0
arund87 <arund87@gmail.com>
2016-05-27 17:06:53 +0530

    PartnerTools updated

--- src/test/java/com/Centurylink/PWCM/PartnerTools.java
+++ src/test/java/com/Centurylink/PWCM/PartnerTools.java
@@ -4,9 +4,11 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import junit.framework.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -19,9 +21,10 @@ public class PartnerTools {
 	ExtentTest test;
 	WebDriver driver;
 	pwcmlibrary config=new pwcmlibrary();
+	String NBIE="NBIE Q Partner Login";
 	
 	
-	@BeforeClass
+	@BeforeMethod
 	   @Parameters("browser")
 	   public void drivercall(String browserName) throws InterruptedException
 	   {
@@ -31,7 +34,7 @@ public class PartnerTools {
 	@Test(priority=0)
 	public void AQCBPage()
 	{
-		extent=new ExtentReports(config.reportpath,false);
+		extent=new ExtentReports(config.reportpath,true);
 	    test=extent.startTest("AQCB page-US55844");
 	    test.assignCategory("PartnerTools");
 	    driver.get(config.toolsurl);
@@ -49,12 +52,13 @@ public class PartnerTools {
 		test.log(LogStatus.PASS, "AQCB Link is present");
 		
 		//User clicks link and is taken to the AQCB login page– open in a new tab
-		driver.findElement(By.linkText("AQCB")).click();
 		String parentw=driver.getWindowHandle();
-		Set<String> s1=driver.getWindowHandles();
-		  Iterator<String> i1=s1.iterator();
-		  while(i1.hasNext()){
-		  String childw=i1.next();
+		driver.findElement(By.linkText("AQCB")).click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		Set<String> aqcbs=driver.getWindowHandles();
+		  Iterator<String> aqcbi=aqcbs.iterator();
+		  while(aqcbi.hasNext()){
+		  String childw=aqcbi.next();
 		  if(!parentw.equalsIgnoreCase(childw))
 		  {
 		  driver.switchTo().window(childw);
@@ -71,12 +75,85 @@ public class PartnerTools {
 		  driver.close();
 				}
 			}
-		driver.switchTo().window(parentw);
+		driver.switchTo().window(parentw);		
 		driver.close();
 		test.log(LogStatus.PASS, "AQCB Test Passed");
 		extent.endTest(test);
 		extent.flush();
 		
 	}
+	
+	@Test(priority=1)
+	public void CNDCpage() throws InterruptedException
+	{
+		extent=new ExtentReports(config.reportpath,false);
+		test=extent.startTest("CNDC (NBIE Portal) - US55845");
+		test.assignCategory("PartnerTools");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.get(config.toolsurl);
+		test.log(LogStatus.PASS, " AEM Homepage Lauched");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.id("submit-button")).isDisplayed();
+		
+		/*PWCMINT login*/
+		config.login(driver);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		test.log(LogStatus.PASS, "AEM Login Success");
+		
+		//Link to the tool can be inserted into a test content page
+		driver.findElement(By.linkText("CNDC")).isDisplayed();
+		test.log(LogStatus.PASS, "CNDC link is present");
+		
+		//User clicks link and is taken to the CNDC login page – open in new tab
+		  String parentw=driver.getWindowHandle();
+		  driver.findElement(By.linkText("CNDC")).click();
+		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		  Set<String> channels=driver.getWindowHandles();
+		  Iterator<String> channeli=channels.iterator();
+		  while(channeli.hasNext()){
+		  String childw=channeli.next();
+		  if(!parentw.equalsIgnoreCase(childw))
+		  {
+		  driver.switchTo().window(childw);
+		  // SSO workaround - Log into the Site for the tool page to display
+		  config.prodlogin(driver);
+		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		  driver.findElement(By.id("welcomeUser")).isDisplayed();
+		  test.log(LogStatus.PASS, "Channel Alliance Log in Successful");
+		  driver.close();			 
+		   }
+	       }
+		  driver.switchTo().window(parentw);
+		  driver.findElement(By.linkText("CNDC")).click();
+		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		    Set<String> cndcs=driver.getWindowHandles();
+		  Iterator<String> cndci=cndcs.iterator();
+			  while(cndci.hasNext()){
+			  String childw=cndci.next();
+			  if(!parentw.equalsIgnoreCase(childw))
+			  {
+			  driver.switchTo().window(childw);
+			  Assert.assertEquals(NBIE, driver.findElement(By.className("title")).getText());
+			  test.log(LogStatus.PASS, "CNDC Page is opened");
+			  
+		 //User sees the CUID Login fields
+			  driver.findElement(By.name("ldapUser")).isDisplayed();
+			  driver.findElement(By.name("password")).isDisplayed();
+			  driver.findElement(By.xpath("//input[@value='    Login    ']")).isDisplayed();
+			  test.log(LogStatus.PASS, "CNDC CUID Log in fields present");
+			  
+		//No other functionality on our part.  Just get the user to the tool login page	  	 				  
+			   driver.close();
+			   
+			   }
+		       }
+		driver.switchTo().window(parentw);
+		driver.close();
+		test.log(LogStatus.PASS, "CNDC Test Passed");
+		extent.endTest(test);
+		extent.flush();
+		}
+	
 
 }
